b4e45d6c1cd1c6a4c6a757abf1a571fc649bc179
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-03-07 23:17:29 -0300

    Modify: App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,52 @@ import stopwatch from './assets/cronometro.png'
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      number:0,
+      button:'Start'
+    }
+    this.timer = null;
+    this.start = this.start.bind(this);
+    this.clean = this.clean.bind(this);
+  }
+
+  start(){
+    let state = this.state
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.button = "Start"
+    }else{
+      this.timer = setInterval( () => {
+        let number = this.state.number;
+        number += 0.1;
+        this.setState({number:number});
+        this.setState({status:true});
+      },100)
+      state.button = "Pause"
+    }
+    this.setState(state);
+  }
+  clean(){
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.number = 0.0;
+    state.button = "Start";
+    this.setState(state)
+  }
   render(){
     return(
       <div className="container">
         <img alt="stopwatch" src={stopwatch} className="img"/>
-        <p className="timer">0.0</p>
+        <p className="timer">{this.state.number.toFixed(1)}</p>
         <div className="buttonArea">
-          <button className="button">VAI</button>
-          <button className="button">LIMPAR</button>
+          <button className="button" onClick={this.start}>{this.state.button}</button>
+          <button className="button" onClick={this.clean}>Clean</button>
         </div>
       </div>
     )
